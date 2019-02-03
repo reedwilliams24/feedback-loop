5701b1f3570de987ae2ae142a6b1ae8cdebf5c86
Reed Williams <reedwilliams24@gmail.com>
2019-02-02 18:54:55 -0800

    set up application (#1)
    * add bower.json file
    
    * add polymer.json
    
    * add package.json
    
    * add index and manifest
    
    * add tests
    
    * add git ignore
    
    * add linkedIn api
    
    * create main page
    
    * create todo api to show how to hook up to mongo
    
    * set up server

---
+++ server.js
@@ -0,0 +1,37 @@
+const express = require('express'),
+      bodyParser = require('body-parser'),
+      MongoClient = require('mongodb').MongoClient,
+      path = require('path'),
+      request = require('request'),
+      app = express(),
+      router = express.Router(),
+      envar = require('envar');
+
+envar.import('env.json');
+
+// TODO: look into not serving up everything
+app.use(express.static(path.join(__dirname, '/')));
+
+MongoClient.connect(envar('mongo_connect_string'), { useNewUrlParser: true }, (err, client) => {
+  if (err) { return console.log(err); }
+
+  app.set('db', client.db('test-db'));
+
+  app.listen(3000, () => {
+    console.log('listening on port 3000');
+  });
+});
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/index.html');
+});
+
+app.use('/api/todo', require('./api/todo.js'));
+
+app.use('/api/linkedin', [
+  // TODO: add security middleware
+  require('./api/linkedin.js')
+]);
